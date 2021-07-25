fb7a771bc7139392dca75c20bebd742a15cc2839
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-07-25 08:01:09 +0000

    Update easy-arch.sh

--- easy-arch.sh
+++ easy-arch.sh
@@ -243,7 +243,7 @@ fi
 
 echo 'zram' > /mnt/etc/modules-load.d/zram.conf
 echo 'options zram num_devices=1' > /mnt/etc/modprobe.d/zram.conf
-echo 'KERNEL=="zram0", ATTR{disksize}="'${ZRAMSIZE}'M" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' > /mnt/etc/udev/rules.d/99-zram.rules
+echo 'KERNEL=="zram0", ATTR{disksize}="'"${ZRAMSIZE}"'M" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' > /mnt/etc/udev/rules.d/99-zram.rules
 echo '# ZRAM' >> /mnt/etc/fstab
 echo '/dev/zram0 					none 		swap 		defaults 0 0' >> /mnt/etc/fstab
 
