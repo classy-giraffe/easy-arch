classy-giraffe/easy-arch

commit 7797b19331780f65eadb63e6f233860e1ca9d2c6

simplify grub & sudoers sed expressions

easy-arch.sh
@@ -324,7 +324,7 @@ EOF
 # Setting up LUKS2 encryption in grub.
 print "Setting up grub config."
 UUID=$(blkid -s UUID -o value $CRYPTROOT)
-sed -i "s,^GRUB_CMDLINE_LINUX=\"\",GRUB_CMDLINE_LINUX=\"rd.luks.name=$UUID=cryptroot root=$BTRFS\",g" /mnt/etc/default/grub
+sed -i "s,^GRUB_CMDLINE_LINUX=\",&rd.luks.name=$UUID=cryptroot root=$BTRFS\",g" /mnt/etc/default/grub
 
 # Configuring the system.    
 arch-chroot /mnt /bin/bash -e <<EOF
@@ -373,7 +373,7 @@ echo "root:$rootpass" | arch-chroot /mnt chpasswd
 if [ -n "$username" ]; then
     print "Adding the user $username to the system with root privilege."
     arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$username"
-    sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers
+    sed -i '%wheel ALL=(ALL) ALL/s/^# //' /mnt/etc/sudoers
     print "Setting user password for $username." 
     echo "$username:$userpass" | arch-chroot /mnt chpasswd
 fi

commit e6daa6f659bbcd180a51a4df895611cae01a08be

remove -R flag from /mnt/root chmod, fix grub sed from 7797b19
grub's sed change in 7797b19 caused the kernel parameters to be enclosed in 3 quotes instead of 2. this fixes it (first delimiter has to be escaped if not /; see GNU sed manual section 4.3)

easy-arch.sh
@@ -326,7 +326,7 @@ mkdir -p /mnt/{home,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
 for subvol in "${subvols[@]:2}"; do # ":2" excludes first two subvols (@snapshots and @var_pkgs) from loop
 mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/"$(sed 's,_,/,g' <<< "$subvol")"
 done
-chmod -R 750 /mnt/root
+chmod 750 /mnt/root
 mount -o $mountopts,subvol=@snapshots $BTRFS /mnt/.snapshots
 mount -o $mountopts,subvol=@var_pkgs $BTRFS /mnt/var/cache/pacman/pkg
 chattr +C /mnt/var/log
@@ -375,7 +375,7 @@ EOF
 # Setting up LUKS2 encryption in grub.
 print "Setting up grub config."
 UUID=$(blkid -s UUID -o value $CRYPTROOT)
-sed -i "s,^GRUB_CMDLINE_LINUX=\",&rd.luks.name=$UUID=cryptroot root=$BTRFS\",g" /mnt/etc/default/grub
+sed -i "\,^GRUB_CMDLINE_LINUX=\"\",s,\",&rd.luks.name=$UUID=cryptroot root=$BTRFS," /mnt/etc/default/grub
 
 # Configuring the system.
 arch-chroot /mnt /bin/bash -e <<EOF