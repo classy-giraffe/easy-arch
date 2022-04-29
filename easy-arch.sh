31acfd3b0c0731c7923427f4b3cdef089f323531
i-c-u-p <96894903+i-c-u-p@users.noreply.github.com>
2022-04-29 05:13:51 +0000

    Update easy-arch.sh

--- easy-arch.sh
+++ easy-arch.sh
@@ -324,7 +324,7 @@ mountopts="ssd,noatime,compress-force=zstd:3,discard=async"
 mount -o $mountopts,subvol=@ $BTRFS /mnt
 mkdir -p /mnt/{home,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
 for subvol in "${subvols[@]:2}"; do # ":2" excludes first two subvols (@snapshots and @var_pkgs) from loop
-mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/$(sed 's,_,/,g' <<< "$subvol")
+mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/"$(sed 's,_,/,g' <<< "$subvol")"
 done
 chmod -R 750 /mnt/root
 mount -o $mountopts,subvol=@snapshots $BTRFS /mnt/.snapshots
