f1138ebf86bd48f88b33ffd26d15c10f502eac29
Tommy <contact@tommytran.io>
2024-06-06 13:07:32 -0700

    Update easy-arch.sh

--- easy-arch.sh
+++ easy-arch.sh
@@ -412,7 +412,7 @@ arch-chroot /mnt /bin/bash -e <<EOF
     locale-gen &>/dev/null
 
     # Create SecureBoot keys. 
-    # This isn't strictly necessary, linux-hardened preset expects it and mkinitcpio will fail without it.
+    # This isn't strictly necessary, linux-hardened preset expects it and mkinitcpio will fail without it
     sbctl create-keys
 
     # Generating a new initramfs.
